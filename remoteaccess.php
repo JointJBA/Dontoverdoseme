9c61284bd944179771ad8ebd7825258ab840ebae
Belal Said <beloskhalil@gmail.com>
2014-10-12 08:48:47 -0400

    extra

--- remoteaccess.php
+++ remoteaccess.php
@@ -9,7 +9,8 @@ $con=mysqli_connect("localhost","root","","mymeds");
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-if($var == 'a') {
+if($var == 'drugs') {
+	$res = $con->query("SELECT brandName, userid, schedule from drugs");
 	
 }
 ?>
