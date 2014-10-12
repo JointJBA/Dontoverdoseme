77d9c046907ec53cfd0b240518051a273e8bbe85
jkrasnits <jkrasnits@gmail.com>
2014-10-12 10:53:22 -0400

    added some of the json stuff

--- index.php
+++ index.php
@@ -74,32 +74,32 @@
           <input type="hidden" name="password" value="<?php echo $password?>">
           <input type="hidden" name="user_id" value="<?php echo $user_id?>">
           
-          <p style="color:white">Take Every:</p>
+          <p style="color:white">Take At:</p>
           <select name="schedule">
-            <option value="1">1 Hour</option>
-            <option value="2">2 Hours</option>
-            <option value="3">3 Hours</option>
-            <option value="4">4 Hours</option>
-            <option value="5">5 Hours</option>
-            <option value="6">6 Hours</option>
-            <option value="7">7 Hours</option>
-            <option value="8">8 Hours</option>
-            <option value="9">9 Hours</option>
-            <option value="10">10 Hours</option>
-            <option value="11">11 Hours</option>
-            <option value="12">12 Hours</option>
-            <option value="13">13 Hours</option>
-            <option value="14">14 Hours</option>
-            <option value="15">15 Hours</option>
-            <option value="16">16 Hours</option>
-            <option value="17">17 Hours</option>
-            <option value="18">18 Hours</option>
-            <option value="19">19 Hours</option>
-            <option value="20">20 Hours</option>
-            <option value="21">21 Hours</option>
-            <option value="22">22 Hours</option>
-            <option value="23">23 Hours</option>
-            <option value="24">24 Hours</option>
+            <option value="1:00">1:00 </option>
+            <option value="2:00">2:00 </option>
+            <option value="3:00">3:00 </option>
+            <option value="4:00">4:00 </option>
+            <option value="5:00">5:00 </option>
+            <option value="6:00">6:00 </option>
+            <option value="7:00">7:00 </option>
+            <option value="8:00">8:00 </option>
+            <option value="9:00">9:00 </option>
+            <option value="10:00">10:00 </option>
+            <option value="11:00">11:00 </option>
+            <option value="12:00">12:00 </option>
+            <option value="13:00">13:00 </option>
+            <option value="14:00">14:00 </option>
+            <option value="15:00">15:00 </option>
+            <option value="16:00">16:00 </option>
+            <option value="17:00">17:00 </option>
+            <option value="18:00">18:00 </option>
+            <option value="19:00">19:00 </option>
+            <option value="20:00">20:00 </option>
+            <option value="21:00">21:00 </option>
+            <option value="22:00">22:00 </option>
+            <option value="23:00">23:00 </option>
+            <option value="24:00">24:00 </option>
           </select>
           
 
@@ -123,7 +123,13 @@
             <?php 
             $count2=2;
             foreach($drugs as $drugrow){
-            echo "<div class='content' id='panel".$count2."'><p>".$drugrow['genName'].$count2."</p></div>";
+            echo "<div class='content' id='panel".$count2."'><p>".$drugrow['genName']."</p></div>";
+
+            $json = file_get_contents('https://api.fda.gov/drug/label.json?search=generic_name:'.$drugrow['genName']);
+            $obj = json_decode($json);
+            echo $obj->results;
+
+
             $count2++;
           }
 ?>
