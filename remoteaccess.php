fb9bd01c789e3840318b6d02e129348f271dbc48
jkrasnits <jkrasnits@gmail.com>
2014-10-12 07:55:02 -0400

    gjkgkj

---
+++ remoteaccess.php
@@ -0,0 +1,15 @@
+<?php
+$var = $_GET['a'];
+
+
+
+$con=mysqli_connect("localhost","root","","mymeds");
+// Check connection
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+if($var == 'a') {
+	
+}
+?>
